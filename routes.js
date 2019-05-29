715f36b72a7595705005b6457a9fd5566831dcfc
matrostik <matrostik@gmail.com>
2019-05-29 10:58:49 +0300

    basic custom activity routes

--- routes.js
+++ routes.js
@@ -3,6 +3,10 @@ const routes = new Router();
 
 // use res.render to load up an ejs view file
 
+/**
+ * Demo pages
+ */
+
 // index page 
 routes.get('/', function(req, res) {
 	res.render('pages/index');
@@ -11,6 +15,113 @@ routes.get('/', function(req, res) {
 // about page 
 routes.get('/about', function(req, res) {
 	res.render('pages/about');
+});
+
+/**
+ * Custom activity routes
+ */
+
+// config.json
+routes.get('/ca/config.json', function(req, res) {
+    console.log("getConfig");
+    var host = 'https://' + req.headers.host;
+    console.log(host);
+
+    var json = {
+        workflowApiVersion: '1.1',
+        metaData: {
+            icon: host + "/images/salesforce-icon-200.png",
+            iconSmall: host + "/images/salesforce-icon-100.png",
+            category: "message",
+            isConfigured: "false",
+            configOnDrop: "true"
+        },
+        type: "REST",
+        lang: {
+            "en-US": {
+                "name": "Basic Custom Activity",
+                "description": "Basic Custom Activity"
+            }
+        },
+        arguments: {
+            execute: {
+                inArguments: [],
+                url: host + "/ca/execute",
+                verb: "POST",
+                body: "",
+                header: "",
+                format: "json",
+                useJwt: "false",
+                timeout: 10000
+            }
+        },
+        configurationArguments: {
+            applicationExtensionKey: "722956e9-3c19-46e6-adab-365d98ca74a4",
+            save: {
+                url: host + "/ca/save",
+                useJwt: "false"
+            },
+            publish: {
+                url: host + "/ca/publish",
+                useJwt: "false"
+            },
+            validate: {
+                url: host + "/ca/validate",
+                useJwt: "false"
+            },
+            stop: {
+                url: host + "/ca/stop",
+                useJwt: "false"
+            }
+        },
+        wizardSteps: [{
+            label: "Step 1",
+            key: "step_1"
+        }],
+        userInterfaces: {
+            configModal: {
+                height: "600",
+                width: "800",
+                url: host + "/ca/ui?numSteps=1"
+            },
+            runningModal: {
+                url: host + "/ca/ui/modal"
+            },
+            runningHover: {
+                url: host + "/ca/ui/hover"
+            }
+        },
+        schema: {
+            arguments: {
+                execute: {
+                    inArguments: [],
+                    outArguments: []
+                }
+            }
+        }
+    };
+	console.log("config.json: ", jsons);
+	return res.status(200).json(json);
+});
+
+// config ui
+routes.get('/ca/ui', function(req, res) {
+	return res.status(200).json({ result: "ui" });
+});
+
+// config ui
+routes.get('/ca/ui/edit', function(req, res) {
+	return res.status(200).json({ result: "ui/edit" });
+});
+
+// runnning modal
+routes.get('/ca/ui/modal', function(req, res) {
+	return res.status(200).json({ result: "ui/modal" });
+});
+
+// running hover
+routes.get('/ca/ui/hover', function(req, res) {
+	return res.status(200).json({ result: "ui/hover" });
 });
 
 export default routes;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import bodyParser from 'body-parser';
 import Routes from './routes';
+import path from 'path';
 
 // load the things we need
 var express = require('express');
@@ -8,9 +9,17 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+/**
+ * VIEW ENGINE
+ */
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
+/**
+ * ROUTES
+ */
+// serve static files
+app.use(express.static(path.join(__dirname, "static")));
 // set routes
 app.use('/', Routes);
 
